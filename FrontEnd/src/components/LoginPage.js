c14d1a3eb14fd28f6446800d36060d23461d9298
Assyl-Salah <salaha@student.mini.pw.edu.pl>
2020-02-02 14:11:16 +0100

    loginpage updated

--- FrontEnd/src/components/LoginPage.js
+++ FrontEnd/src/components/LoginPage.js
@@ -4,9 +4,8 @@ import { Container, Row, Col, Input, Button, FormGroup, Label}
     from 'reactstrap';
 import {withRouter} from "react-router";
 import AuthHelperMethods from "./AuthHelperMethods";
-import {connect} from "react-redux";
-
-
+import AuthenticationLogin from "./AuthenticationLogin";
+import Alert from "reactstrap/es/Alert";
 const Auth = new AuthHelperMethods();
 
 class LoginPage extends Component {
@@ -15,30 +14,20 @@ class LoginPage extends Component {
         this.state = {
             login: "",
             password: "",
-            errorp : " "
+            isValid : false,
+            loginfailed : false,
+            error: false
         };
         this.handleChangeLogin = this.handleChangeLogin.bind(this);
         this.handleChangePassword = this.handleChangePassword.bind(this);
-
+        this.formvalid=this.formvalid.bind(this);
+      // this.SubmitButton=this.SubmitButton.bind(this);
         this.logIn = this.logIn.bind(this);
     }
 
-    validatePassword=(value)=>{
-
-
-        if (value.length<8)
-        {
-            this.setState({ errorp:'Password must be at least 8 characters !!' })
-        }
-        else
-        {
-            this.setState({ errorp: '' })
-        }
-
-
+    formvalid(){
+        return this.state.login.length >0 && this.state.password.length>0
     }
-
-
     render() {
         return (
             <div className='login-page'>
@@ -46,24 +35,27 @@ class LoginPage extends Component {
                     <Container className="main-layout-page-wrapper themed-container">
                         <Row>
                             <Col className="content" sm="12" md={{ size: 6, offset: 3 }}>
-                                <form className='form'>
+                                <form className='form' onSubmit={this.logIn}>
+                                    {this.state.loginfailed &&
+                                    <Alert variant="danger" show={true}>
+                                        Incorrect Username or Password!! try again
+                                    </Alert>}
                                     <FormGroup>
                                         <h2 className='nameOfPage'>
                                             Sign in
                                         </h2>
                                     </FormGroup>
                                     <FormGroup>
-                                        <Label>Email
-                                            <Input type="text" value={this.state.login} onChange={this.handleChangeLogin} />
+                                        <Label>Login
+                                            <Input type="text" value={this.state.login} onChange={this.handleChangeLogin}/>
                                         </Label>
                                     </FormGroup>
                                     <FormGroup>
                                         <Label>Password
-                                            <Input type="password" value={this.state.password} onChange={this.handleChangePassword}  />
-                                            {/*{!this.state.validatePassword ? null : <label>wrong Password</label>}*/}
+                                            <Input type="password" value={this.state.password} onChange={this.handleChangePassword}/>
                                         </Label>
                                     </FormGroup>
-                                    <Button color="success" size="lg" onClick={this.logIn}> Submit </Button>
+                                    <Button color="success" size="lg" disabled={!this.formvalid()} > Submit </Button>
                                 </form>
                             </Col>
                         </Row>
@@ -72,64 +64,31 @@ class LoginPage extends Component {
             </div>
         )
     }
-
     handleChangeLogin(e) {
         this.setState({
             login: e.target.value
         })
     }
-
     handleChangePassword(e) {
         this.setState({
             password: e.target.value
         })
     }
-
-    logIn() {
-        //TODO - not working, also need to be changed to more secure - add token also
-    //     let user = {login: this.state.login,  password: this.state.password}
-    //     fetch("http://localhost:8080/users", {
-    //         method: 'POST',
-    //         headers: {
-    //             Accept: 'application/json',
-    //             'Content-Type': 'application/json',
-    //         },
-    //         body: JSON.stringify(user)
-    //     })
-    //         .then(res => {
-    //                 if (res.status === 200) {
-    //                     this.props.history.push("/home");
-    //                 }
-    //             }
-    //         );
-    //
+    logIn(e) {
+        e.preventDefault();
         Auth.login(this.state.login, this.state.password)
             .then(res => {
-                if (res === false) {
-                    return alert("Sorry those credentials don't exist!");
-                }
-                this.props.history.replace("/");
+         // Auth.loggedIn(this.state.login , this.state.password,res.data.token)
+
+                this.props.history.push("/home");
             })
-            .catch(err => {
-                alert(err);
+            .catch(() => {
+               this.setState({error:false})
+                this.setState({loginfailed:true})
+
             });
-        this.props.history.push("/home");
+
     }
 
 }
-
-const mapStateToProps = state => {
-    return {
-        // user: state.user
-    };
-};
-
-const mapDispatchToProps = dispatch => ({
-    // fetchuser: uname => dispatch(fetchuser(uname))
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(withRouter(LoginPage));
-
+export default (withRouter(LoginPage));
